8b299c7e8f3d7608acf0f124c745969535b2b1a4
Ryan Zheng <nogrethumphrey@gmail.com>
2023-07-18 12:53:36 +0200

    stop the event from popping up when click child directories

--- src/components/fileExplorer/FileItem.vue
+++ src/components/fileExplorer/FileItem.vue
@@ -1,37 +1,51 @@
 <template>
-    <div :class="{ 'folder': !file.is_file, 'open': isOpen.value }" @click="toggle" class="file-item">
-        <div class="icon">
-            <svg v-if="!file.is_file" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#87CEEB" class="folder-icon">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H6a2 2 0 01-2-2V5a2 2 0 011.293-1.858l3.704-1.48A2 2 0 0110 2h10a2 2 0 012 2v7M9 16a2 2 0 00-2 2v3a2 2 0 002 2h6a2 2 0 002-2v-3a2 2 0 00-2-2H9z"/>
-            </svg>
-            <i v-else class="fas fa-file"></i>
+    <div :class="{ 'folder': !file.is_file, 'open': isFileOpen() }" @click.stop="toggle" class="file-item">
+        <div class="file-header">
+            <div class="icon">
+                <svg v-if="!file.is_file" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#87CEEB" class="folder-icon">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H6a2 2 0 01-2-2V5a2 2 0 011.293-1.858l3.704-1.48A2 2 0 0110 2h10a2 2 0 012 2v7M9 16a2 2 0 00-2 2v3a2 2 0 002 2h6a2 2 0 002-2v-3a2 2 0 00-2-2H9z"/>
+                </svg>
+                <i v-else class="fas fa-file"></i>
+            </div>
+            {{ file.name }}
         </div>
-        {{ file.name }}
         <transition name="folder">
-            <div v-if="!file.is_file && isOpen.value" class="children">
+            <div v-if="!file.is_file && isFileOpen()" class="children">
                 <FileItem v-for="(child, index) in file.children" :key="`${child.path}-${index}`" :file="child"/>
             </div>
         </transition>
     </div>
 </template>
 
-
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { onMounted, watch,reactive } from 'vue';
 import { TreeNode } from '../../utils/fileExplorer/TreeNode';
 import FileItem from './FileItem.vue';
 
 const props = defineProps<{ file: TreeNode }>();
 
-const isOpen = ref(false);
+// Create a Map to store open states for each file
+const openStates = reactive(new Map());
+
 const toggle = () => {
     if (!props.file.is_file) {
-        console.log(`Toggled '${props.file.name}' from ${isOpen.value} to ${!isOpen.value}`);
-        console.log(`Is directory? ${!props.file.is_file}`);
-        isOpen.value = !isOpen.value;
-        console.log(`Is open? ${isOpen.value}`);
+        // Use the file name as the key for the Map
+        const isOpen = openStates.get(props.file.name);
+        openStates.set(props.file.name, !isOpen);
     }
 }
+
+const isFileOpen = () => {
+    return openStates.get(props.file.name);
+}
+
+watch(isFileOpen, (newValue, oldValue) => {
+    if (newValue) {
+        console.log(`'${props.file.name}' is now open. Children:`, props.file.children);
+    } else {
+        console.log(`'${props.file.name}' is now closed.`);
+    }
+});
 
 onMounted(() => {
     console.log("File item:", props.file);
@@ -44,12 +58,16 @@ onMounted(() => {
 <style scoped>
 .file-item {
     display: flex;
-    align-items: center;
+    flex-direction: column;
     cursor: pointer;
     margin-bottom: 0.5rem;
     color: #000;  
 }
 
+.file-header {
+    display: flex;
+    align-items: center;
+}
 .file-item:hover {
     color: #000;  
 }
