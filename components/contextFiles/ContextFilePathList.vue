1f3249019a4a02f49cb2dc3f23ae342663c44a6f
Ryan Zheng <nogrethumphrey@gmail.com>
2024-10-15 07:30:27 +0200

    use custom x icon for remove

--- components/contextFiles/ContextFilePathList.vue
+++ components/contextFiles/ContextFilePathList.vue
@@ -45,12 +45,14 @@
             </div>
             <button 
               @click.stop="removeContextFilePath(index)" 
-              class="text-white bg-red-500 hover:bg-red-600 transition-colors duration-300 p-2.5 rounded-full focus:outline-none focus:ring-2 focus:ring-red-500"
+              class="text-red-500 hover:text-white hover:bg-red-500 transition-colors duration-300 p-1 rounded-full focus:outline-none focus:ring-2 focus:ring-red-500"
               title="Remove this file"
               aria-label="Remove file"
               :disabled="uploadingFiles.includes(filePath.path)"
             >
-              <i class="fas fa-times"></i>
+              <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M18 6L6 18M6 6L18 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
             </button>
           </li>
         </ul>
