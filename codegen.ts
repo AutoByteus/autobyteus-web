ba19847397987f61087a501d093577f03eb05b1c
Ryan Zheng <nogrethumphrey@gmail.com>
2025-10-21 07:29:37 +0200

    fix the BACKEND_GRAPHQL_BASE_URL variable in the codegen

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: process.env.NUXT_PUBLIC_GRAPHQL_BASE_URL,
+  schema: process.env.BACKEND_GRAPHQL_BASE_URL || process.env.NUXT_PUBLIC_GRAPHQL_BASE_URL,
   documents: "graphql/**/*.ts",
   generates: {
     "./generated/graphql.ts": {
